d72c562cf8809c75a3001e0449f601ac0fdf85cf
qepri.xyz <qepri.xyz@gmail.com>
2023-08-19 13:06:57 -0600

    circle photo

--- assets/js/sigil_magick.js
+++ assets/js/sigil_magick.js
@@ -15,3 +15,5 @@
    .modulateScale(osc(10, 0),-0.03)
    .rotate(() => Math.PI * mouse.y /270)
    .scale(0.8, () => (1.05 + 0.1 * Math.sin(0.5*time))).out(o0)
+
+// hydra.js is made by olivia.jack.xyz
